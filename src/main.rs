4c964d55ca93f6aa5dd25ef835f74fab81bb264f
Nathan Franck <nathanfranck1@gmail.com>
2022-08-27 21:56:00 -0600

    UI experiments, simpler collisions

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use bevy::{ecs::component, prelude::*};
+use bevy::prelude::*;
 use bevy_inspector_egui::{Inspectable, RegisterInspectable, WorldInspectorPlugin};
 use bevy_rapier3d::prelude::*;
 use rand::prelude::random;
@@ -21,13 +21,14 @@ fn main() {
         .add_system(gamepad_system)
         .add_startup_system(setup)
         .add_startup_system(setup_physics)
-        .add_plugin(WorldInspectorPlugin::new())
+        // .add_plugin(WorldInspectorPlugin::new())
         .add_system(kill_player)
         .add_system(follow_cam)
         .add_system(player_collectables)
         .add_system(start_the_party)
         .add_system(party)
         .add_system(game_over_checker)
+        .add_startup_system(setup_ui)
         .insert_resource(GameResources {
             party_material: StandardMaterial {
                 base_color: Color::Rgba {
@@ -39,17 +40,294 @@ fn main() {
                 ..default()
             },
         })
-        .register_inspectable::<Toggles>()
+        // .register_inspectable::<Toggles>()
         .run();
 }
 
 #[derive(Component, Inspectable)]
 struct Toggles {}
 
+fn setup_ui(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands
+        .spawn_bundle(NodeBundle {
+            style: Style {
+                size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
+                justify_content: JustifyContent::SpaceBetween,
+                ..default()
+            },
+            color: Color::NONE.into(),
+            ..default()
+        })
+        .with_children(|parent| {
+            // left vertical fill (border)
+            parent
+                .spawn_bundle(NodeBundle {
+                    style: Style {
+                        size: Size::new(Val::Px(200.0), Val::Percent(100.0)),
+                        border: UiRect::all(Val::Px(2.0)),
+                        ..default()
+                    },
+                    color: Color::rgb(0.65, 0.65, 0.65).into(),
+                    ..default()
+                })
+                .with_children(|parent| {
+                    // left vertical fill (content)
+                    parent
+                        .spawn_bundle(NodeBundle {
+                            style: Style {
+                                size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
+                                align_items: AlignItems::Center,
+                                ..default()
+                            },
+                            color: Color::rgb(0.15, 0.15, 0.15).into(),
+                            ..default()
+                        })
+                        .with_children(|parent| {
+                            // text
+                            parent.spawn_bundle(
+                                TextBundle::from_section(
+                                    "Collect All The Animals!",
+                                    TextStyle {
+                                        font: asset_server.load("FredokaOne-Regular.ttf"),
+                                        font_size: 30.0,
+                                        color: Color::WHITE,
+                                    },
+                                )
+                                .with_style(Style {
+                                    margin: UiRect::all(Val::Px(5.0)),
+                                    ..default()
+                                }),
+                            );
+                        });
+                });
+            // right vertical fill
+            parent
+                .spawn_bundle(NodeBundle {
+                    style: Style {
+                        flex_direction: FlexDirection::ColumnReverse,
+                        justify_content: JustifyContent::Center,
+                        size: Size::new(Val::Px(200.0), Val::Percent(100.0)),
+                        ..default()
+                    },
+                    color: Color::rgb(0.15, 0.15, 0.15).into(),
+                    ..default()
+                })
+                .with_children(|parent| {
+                    // Title
+                    parent.spawn_bundle(
+                        TextBundle::from_section(
+                            "Scrolling list",
+                            TextStyle {
+                                font: asset_server.load("FredokaOne-Regular.ttf"),
+                                font_size: 25.,
+                                color: Color::WHITE,
+                            },
+                        )
+                        .with_style(Style {
+                            size: Size::new(Val::Undefined, Val::Px(25.)),
+                            margin: UiRect {
+                                left: Val::Auto,
+                                right: Val::Auto,
+                                ..default()
+                            },
+                            ..default()
+                        }),
+                    );
+                    // List with hidden overflow
+                    parent
+                        .spawn_bundle(NodeBundle {
+                            style: Style {
+                                flex_direction: FlexDirection::ColumnReverse,
+                                align_self: AlignSelf::Center,
+                                size: Size::new(Val::Percent(100.0), Val::Percent(50.0)),
+                                overflow: Overflow::Hidden,
+                                ..default()
+                            },
+                            color: Color::rgb(0.10, 0.10, 0.10).into(),
+                            ..default()
+                        })
+                        .with_children(|parent| {
+                            // Moving panel
+                            parent
+                                .spawn_bundle(NodeBundle {
+                                    style: Style {
+                                        flex_direction: FlexDirection::ColumnReverse,
+                                        flex_grow: 1.0,
+                                        max_size: Size::new(Val::Undefined, Val::Undefined),
+                                        ..default()
+                                    },
+                                    color: Color::NONE.into(),
+                                    ..default()
+                                })
+                                .with_children(|parent| {
+                                    // List items
+                                    for i in 0..30 {
+                                        parent.spawn_bundle(
+                                            TextBundle::from_section(
+                                                format!("Item {i}"),
+                                                TextStyle {
+                                                    font: asset_server
+                                                        .load("FredokaOne-Regular.ttf"),
+                                                    font_size: 20.,
+                                                    color: Color::WHITE,
+                                                },
+                                            )
+                                            .with_style(Style {
+                                                flex_shrink: 0.,
+                                                size: Size::new(Val::Undefined, Val::Px(20.)),
+                                                margin: UiRect {
+                                                    left: Val::Auto,
+                                                    right: Val::Auto,
+                                                    ..default()
+                                                },
+                                                ..default()
+                                            }),
+                                        );
+                                    }
+                                });
+                        });
+                });
+            // absolute positioning
+            parent
+                .spawn_bundle(NodeBundle {
+                    style: Style {
+                        size: Size::new(Val::Px(200.0), Val::Px(200.0)),
+                        position_type: PositionType::Absolute,
+                        position: UiRect {
+                            left: Val::Px(210.0),
+                            bottom: Val::Px(10.0),
+                            ..default()
+                        },
+                        border: UiRect::all(Val::Px(20.0)),
+                        ..default()
+                    },
+                    color: Color::rgb(0.4, 0.4, 1.0).into(),
+                    ..default()
+                })
+                .with_children(|parent| {
+                    parent.spawn_bundle(NodeBundle {
+                        style: Style {
+                            size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
+                            ..default()
+                        },
+                        color: Color::rgb(0.8, 0.8, 1.0).into(),
+                        ..default()
+                    });
+                });
+            // render order test: reddest in the back, whitest in the front (flex center)
+            parent
+                .spawn_bundle(NodeBundle {
+                    style: Style {
+                        size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
+                        position_type: PositionType::Absolute,
+                        align_items: AlignItems::Center,
+                        justify_content: JustifyContent::Center,
+                        ..default()
+                    },
+                    color: Color::NONE.into(),
+                    ..default()
+                })
+                .with_children(|parent| {
+                    parent
+                        .spawn_bundle(NodeBundle {
+                            style: Style {
+                                size: Size::new(Val::Px(100.0), Val::Px(100.0)),
+                                ..default()
+                            },
+                            color: Color::rgb(1.0, 0.0, 0.0).into(),
+                            ..default()
+                        })
+                        .with_children(|parent| {
+                            parent.spawn_bundle(NodeBundle {
+                                style: Style {
+                                    size: Size::new(Val::Px(100.0), Val::Px(100.0)),
+                                    position_type: PositionType::Absolute,
+                                    position: UiRect {
+                                        left: Val::Px(20.0),
+                                        bottom: Val::Px(20.0),
+                                        ..default()
+                                    },
+                                    ..default()
+                                },
+                                color: Color::rgb(1.0, 0.3, 0.3).into(),
+                                ..default()
+                            });
+                            parent.spawn_bundle(NodeBundle {
+                                style: Style {
+                                    size: Size::new(Val::Px(100.0), Val::Px(100.0)),
+                                    position_type: PositionType::Absolute,
+                                    position: UiRect {
+                                        left: Val::Px(40.0),
+                                        bottom: Val::Px(40.0),
+                                        ..default()
+                                    },
+                                    ..default()
+                                },
+                                color: Color::rgb(1.0, 0.5, 0.5).into(),
+                                ..default()
+                            });
+                            parent.spawn_bundle(NodeBundle {
+                                style: Style {
+                                    size: Size::new(Val::Px(100.0), Val::Px(100.0)),
+                                    position_type: PositionType::Absolute,
+                                    position: UiRect {
+                                        left: Val::Px(60.0),
+                                        bottom: Val::Px(60.0),
+                                        ..default()
+                                    },
+                                    ..default()
+                                },
+                                color: Color::rgb(1.0, 0.7, 0.7).into(),
+                                ..default()
+                            });
+                            // alpha test
+                            parent.spawn_bundle(NodeBundle {
+                                style: Style {
+                                    size: Size::new(Val::Px(100.0), Val::Px(100.0)),
+                                    position_type: PositionType::Absolute,
+                                    position: UiRect {
+                                        left: Val::Px(80.0),
+                                        bottom: Val::Px(80.0),
+                                        ..default()
+                                    },
+                                    ..default()
+                                },
+                                color: Color::rgba(1.0, 0.9, 0.9, 0.4).into(),
+                                ..default()
+                            });
+                        });
+                });
+            // bevy logo (flex center)
+            parent
+                .spawn_bundle(NodeBundle {
+                    style: Style {
+                        size: Size::new(Val::Percent(100.0), Val::Percent(100.0)),
+                        position_type: PositionType::Absolute,
+                        justify_content: JustifyContent::Center,
+                        align_items: AlignItems::FlexEnd,
+                        ..default()
+                    },
+                    color: Color::NONE.into(),
+                    ..default()
+                })
+                .with_children(|parent| {
+                    // bevy logo (image)
+                    parent.spawn_bundle(ImageBundle {
+                        style: Style {
+                            size: Size::new(Val::Px(500.0), Val::Auto),
+                            ..default()
+                        },
+                        image: asset_server.load("branding/bevy_logo_dark_big.png").into(),
+                        ..default()
+                    });
+                });
+        });
+}
+
 fn party(time: Res<Time>, mut party_zone: Query<(&mut Transform, &PartyZone)>) {
     for (mut transform, party_zone) in party_zone.iter_mut() {
         let displacement = (time.seconds_since_startup() * 7.).sin().powf(1.).abs() * 1.;
-        print!("Movin!\n {}", displacement);
+        // print!("Movin!\n {}", displacement);
         transform.translation = party_zone.bob_position + Vec3::Y * displacement as f32;
     }
 }
@@ -187,7 +465,7 @@ fn start_the_party(
     game_resources: Res<GameResources>,
     mut materials: ResMut<Assets<StandardMaterial>>,
     mut collisions: EventReader<CollisionEvent>,
-    mut players: Query<&Children, With<Player>>,
+    players: Query<&Children, With<Player>>,
     party_zones: Query<(), With<PartyZone>>,
     mut material_handles: Query<Entity, With<Handle<StandardMaterial>>>,
 ) {
@@ -205,7 +483,8 @@ fn start_the_party(
                                 for &child in children.iter() {
                                     match material_handles.get_mut(child) {
                                         Ok(material_entity) => {
-                                            let party_material = materials.add(game_resources.party_material.clone());
+                                            let party_material = materials
+                                                .add(game_resources.party_material.clone());
                                             commands.entity(material_entity).insert(party_material);
                                         }
                                         Err(_) => {}
@@ -283,12 +562,12 @@ fn connect_from_scene(
         });
     }
 
-    for (entity, name, _) in collectables.chain(bunnies) {
+    for (entity, _, _) in collectables.chain(bunnies) {
         commands
             .entity(entity)
             .insert(RigidBody::Dynamic)
             .insert(Velocity { ..default() })
-            .insert(Collider::cylinder(1.0, 2.0))
+            .insert(Collider::ball(2.0))
             .insert(Restitution::coefficient(0.2))
             .insert(LockedAxes::ROTATION_LOCKED)
             .insert(ActiveEvents::COLLISION_EVENTS)
@@ -297,6 +576,7 @@ fn connect_from_scene(
                 coefficient: 0.,
                 ..default()
             })
+            // .insert(Ccd::enabled()) // Breaks with TriMesh :(
             .insert(Collectable {});
     }
     let level = named_entities_with_children
